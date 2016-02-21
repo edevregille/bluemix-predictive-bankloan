09536d94e76f65edf22a7e703c527f5fc0a63789
edevregille <manudevregille@gmail.com>
2016-02-21 18:27:54 +0000

    Update form-service.js

--- app/views/js/services/form-service.js
+++ app/views/js/services/form-service.js
@@ -10,8 +10,7 @@ var appServices = angular.module('appServices',[]);
 				var input = {
 					tablename: 'Bank_loan.txt',
 					header: [ 'age', 'ed', 'employ', 'address', 'income', 'debtinc', 'creddebt', 'othdebt', 'default'],
-					data: [[ formData.age, formData.ed, formData.employ, formData.address, formData.income, formData.debtinc, formData.creddebt, formData.othdebt,'1' ]]
-					//data: [[ 30, 5, 10, 5, 10, 2, 2, 2, 0 ]]
+					data: [[ formData.age, formData.ed, formData.employ, formData.address, formData.income, formData.debtinc, formData.creddebt, formData.othdebt,formData.defaultpayment ]]
 				};
 				return $http.post('api/score', {contextID: contextID, input: input})
 					.success(function(data, status, headers, config) {
